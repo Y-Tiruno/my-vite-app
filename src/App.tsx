ae8552245046bb43e7dfa8961d86da18e052eecf
Y-Tiruno <tiruno.koumakyou.9@gmail.com>
2026-06-24 02:17:41 +0000

    commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-blue-700 text-white h-screen flex items-center justify-center text-4xl">
+      こんにちは
+    </div>
+  )
+}
+
+export default App
